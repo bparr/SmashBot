8d1dbebc545f6747b078df7e033c2bf1644e42a8
Ben Parr <bparr@bparr.com>
2017-11-08 20:46:09 -0500

    Prep for copy code hack.

--- globals.py
+++ globals.py
@@ -8,10 +8,16 @@ def init(dolphin, smashbot_port, opponent_port):
     controller = melee.controller.Controller(port=smashbot_port, dolphin=dolphin)
     global opponent_controller
     opponent_controller = melee.controller.Controller(port=opponent_port, dolphin=dolphin)
+    global controller2
+    controller2 = opponent_controller
     global smashbot_state
     smashbot_state = gamestate.player[smashbot_port]
+    global opponent_state2
+    opponent_state2 = smashbot_state
     global opponent_state
     opponent_state = gamestate.player[opponent_port]
+    global smashbot_state2
+    smashbot_state2 = opponent_state
     global framedata
     framedata = melee.framedata.FrameData()
     global logger
